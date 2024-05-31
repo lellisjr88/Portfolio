ea79ad9bb5b12723dca86d1651e84d9c23a4b308
lellisjr88 <elsj1988@gmail.com>
2024-05-31 17:55:56 -0300

    Seção Sobre

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,53 @@
-const button = document.querySelector('#iconMenu')
-const menu = document.querySelector('.nav')
+const button = document.querySelector("#iconMenu");
+const menu = document.querySelector(".nav");
 
-button.addEventListener('click',openMenu);
+button.addEventListener("click", openMenu);
 
 function openMenu() {
-    menu.classList.toggle('open');
+  menu.classList.toggle("open");
 }
 
 window.addEventListener("resize", function () {
-    if (window.innerWidth >= 768) {
-      menu.classList.remove("open");
-    }
+  if (window.innerWidth >= 768) {
+    menu.classList.remove("open");
+  }
+});
+
+/*Secão Sobre mim */
+
+const buttonSobre = document.querySelector("#buttonSobre");
+const buttonHabilidade = document.querySelector("#buttonHabilidades");
+const buttonEducacao = document.querySelector("#buttonEducacao");
+
+const sobreMim = document.querySelector(".sobreMim");
+const habilidadesText = document.querySelector(".habilidadesText");
+const educacao = document.querySelector(".educacao");
+
+buttonHabilidade.addEventListener("click", function () {
+  habilidadesText.style.display = "block";
+  sobreMim.style.display = "none";
+  educacao.style.display = "none";
+  if ((habilidadesText.style.display = "none")) {
+    habilidadesText.style.display = "block";
+  }
+});
+
+buttonEducacao.addEventListener("click", function () {
+  educacao.style.display = "block";
+  sobreMim.style.display = "none";
+  habilidadesText.style.display = "none";
+
+  if ((educacao.style.display = "none")) {
+    educacao.style.display = "block";
+  }
+});
+
+buttonSobre.addEventListener("click", function () {
+  sobreMim.style.display = "block";
+  educacao.style.display = "none";
+  habilidadesText.style.display = "none";
+
+  if ((sobreMim.style.display = "none")) {
+    sobreMim.style.display = "block";
+  }
 });
