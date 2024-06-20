aea099c17d6e3519face0d52dc144ea1dc1b1fe8
Seu nome aqui <elsj1988@gmail.com>
2024-06-20 15:36:31 -0300

    Inclusão do Glider Carroussel

--- scripts/index.js
+++ scripts/index.js
@@ -143,3 +143,43 @@ emailForm.addEventListener('submit', function (event) {
 // }
 
 //Footer Date
+
+// Responsive carousel
+// =============================================================================
+
+const $responsiveCarousel = document.querySelector(".js-carousel--responsive");
+
+new Glider($responsiveCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  duration: 0.25,
+  dots: ".js-carousel--responsive-dots",
+  arrows: {
+    prev: ".js-carousel--responsive-prev",
+    next: ".js-carousel--responsive-next",
+  },
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    }
+  ],
+});
